261fc293fa4ef800ea50ff45e98c23693c7e5dc4
Azael Rivera <azaelkiller@gmail.com>
2021-03-16 00:09:50 -0700

    updated navbar design, logo

--- src/app/templates/dialogs/confirm-dialog/confirm-dialog.module.ts
+++ src/app/templates/dialogs/confirm-dialog/confirm-dialog.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from './confirm-dialog.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [ConfirmDialogComponent],
-  imports: [CommonModule, MatDialogModule],
+  imports: [CommonModule, MatDialogModule, MatIconModule],
   exports: [ConfirmDialogComponent],
 })
 export class ConfirmDialogModule {}
